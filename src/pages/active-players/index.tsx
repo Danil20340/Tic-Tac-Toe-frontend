8e9f95d7f74a8697e3a6ce33c717e465a32e505b
Даниил <144549184+Danil20340@users.noreply.github.com>
2024-10-27 22:41:31 +0500

    Add new page "Active"

--- src/pages/active-players/index.tsx
+++ src/pages/active-players/index.tsx
@@ -1,7 +1,51 @@
 import React from 'react'
-
+import './index.css'
+import { Container } from '../../components/container';
+import { Text } from '../../components/text';
+import { Button } from '../../components/button';
+import { Status } from '../../components/status';
+import { Row } from '../../components/row';
+import { Cell } from '../../components/cell';
+import { Switch } from '../../components/switch';
 export const ActivePlayers = () => {
+
+  const playerActive = [
+    { FIO: 'Александров Игнат Анатолиевич', status: "Свободен" },
+    { FIO: 'Василенко Эрик Платонович', status: "В игре" },
+    { FIO: 'Быков Юрий Виталиевич', status: "В игре" },
+    { FIO: 'Галкин Феликс Платонович', status: "В игре" },
+    { FIO: 'Комаров Цефас Александрович', status: "В игре" },
+    { FIO: 'Шевченко Рафаил Михайлович', status: "В игре" },
+    { FIO: 'Гордеев Шамиль Леонидович', status: "Свободен" },
+    { FIO: 'Бобров Фёдор Викторович', status: "Свободен" },
+    { FIO: 'Суворов Феликс Григорьевич', status: "В игре" },
+    { FIO: 'Марков Йошка Фёдорович', status: "Свободен" }
+
+  ]
+  const elements = playerActive.map(item =>
+    <Row style={{ boxShadow: "none", padding: "12px 0px" }}>
+      <Cell style={{ width: "360px" }}>{item.FIO}</Cell>
+      {item.status === "Свободен" ? <Status isFree={true} /> : <Status />}
+      {item.status === "Свободен" ? <Button >Позвать играть</Button> :
+        <Button disabled={true} backgroundColor="#F7F7F7" >Позвать играть</Button>}
+    </Row>
+  );
   return (
-    <div>Active players</div>
+
+    <Container className="active-players">
+      <Container style={{ justifyContent: 'space-between' }}>
+        <Text style={{ fontWeight: 700, fontSize: "24px", lineHeight: '36px' }}>Активные игроки</Text>
+        <Container style={{ width: '', alignItems: 'center' }}>
+          <Text style={{ marginRight: '8px' }}>Только свободные</Text>
+          <Container style={{ width: '' }}>
+            <Switch />
+          </Container>
+        </Container>
+      </Container>
+      <Container style={{ flexDirection: "column", alignItems: 'flex-start' }}>
+        {elements}
+      </Container>
+    </Container>
+
   )
 }
