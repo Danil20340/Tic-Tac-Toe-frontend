f9a1b00020a056b7abf0b87d769a5c19c32cf554
Даниил <144549184+Danil20340@users.noreply.github.com>
2024-12-10 02:48:23 +0500

    The players page is linked to the server

--- src/pages/players/index.tsx
+++ src/pages/players/index.tsx
@@ -1,39 +1,52 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import block from '../../assets/block.svg'
 import woman from '../../assets/woman.svg'
 import man from '../../assets/man.svg'
-import close from '../../assets/close.svg'
 import { Container } from '../../components/container'
 import { Text } from '../../components/text'
+import './index.css'
 import { Button } from '../../components/button'
 import { Row } from '../../components/row'
 import { Cell } from '../../components/cell'
 import { Status } from '../../components/status'
 import { AddPlayerModal } from '../../components/add-player-modal'
-import { ModalTemplate } from '../../components/modal-template'
+import { useChangePlayerStatusMutation, useGetAllPlayersQuery } from '../../app/services/playerApi'
+import { formatToClientDate } from '../../utils/format-to-client-date'
+import { hasErrorField } from '../../utils/has-error-field'
+import { useModal } from '../../components/modal-context'
 
 export const Players = () => {
-  const [AddPlayerActive, setAddPlayerActive] = useState(false)
-
-  const playerList = [
-    { FIO: 'Александров Игнат Анатолиевич', age: "24", gender: "woman", state: "blocked", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "unlock" },
-    { FIO: 'Мартынов Остап Фёдорович', age: "12", gender: "woman", state: "active", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "block" },
-    { FIO: 'Комаров Цефас Александрович', age: "83", gender: "man", state: "active", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "block" },
-    { FIO: 'Кулаков Станислав Петрович', age: "43", gender: "man", state: "blocked", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "unlock" },
-    { FIO: 'Борисов Йошка Васильевич', age: "32", gender: "woman", state: "active", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "block" },
-    { FIO: 'Негода Михаил Эдуардович', age: "33", gender: "man", state: "active", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "block" },
-    { FIO: 'Жданов Зураб Алексеевич', age: "24", gender: "man", state: "active", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "block" },
-    { FIO: 'Бобров Фёдор Викторович', age: "19", gender: "man", state: "blocked", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "unlock" },
-    { FIO: 'Александров Игнат Анатолиевич', age: "21", gender: "woman", state: "active", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "block" },
-    { FIO: 'Многогрешный Павел Виталиевич', age: "24", gender: "man", state: "blocked", generated: "12 октября 2021", changed: "22 октября 2021", blocked_changed: "unlock" }
-  ]
-
+  const { data, refetch } = useGetAllPlayersQuery();
+  const [selectPlayer, setSelectPlayer] = useState('')
+  const [modalState, setModalState] = useState<'ADD' | 'EDIT'>('ADD')
+  const [changePlayerStatus] = useChangePlayerStatusMutation();
+  const [error, setError] = useState('');
+  const handleClick = async (id: string) => {
+    try {
+      await changePlayerStatus({ id }).unwrap();
+      await refetch();
+    } catch (error) {
+      if (hasErrorField(error)) {
+        setError(error.data.error)
+      } else {
+        setError(error as string)
+      }
+    }
+  }
+  const handleAddPlayer = async () => {
+    try {
+      await refetch();
+    } catch (error) {
+      console.error("Ошибка при обновлении данных:", error);
+    }
+  };
+  const { openModal } = useModal();
   return (
     <>
       <Container style={{ maxWidth: '1416px' }} className="rating-players">
         <Container style={{ justifyContent: 'space-between' }}>
           <Text style={{ fontWeight: 700, fontSize: "24px", lineHeight: '36px' }}>Список игроков</Text>
-          <Button onClick={() => setAddPlayerActive(true)}>Добавить игрока</Button>
+          <Button onClick={() => { openModal('playerModal'); setModalState(selectPlayer ? 'EDIT' : 'ADD') }}>{selectPlayer ? 'Редактировать игрока' : 'Добавить игрока'}</Button>
         </Container>
         <Container style={{ flexDirection: "column", alignItems: 'flex-start' }}>
           <Row>
@@ -45,27 +58,62 @@ export const Players = () => {
             <Cell style={{ width: '144px' }} >Изменен</Cell>
             <Cell style={{ width: '174px' }}></Cell>
           </Row>
-          {playerList.map(item =>
-            <Row style={{ alignItems: 'center', height: '64px' }}>
-              <Cell style={{ width: "380px" }}>{item.FIO}</Cell>
-              <Cell>{item.age}</Cell>
-              {item.gender === "woman" ? <Cell style={{ width: '46px' }}><img src={woman} alt="" />
-              </Cell> : <Cell style={{ width: '46px' }}><img src={man} alt="" /></Cell>}
-              {item.state === "blocked" ? <Status style={{ width: '144px' }} status='isBlock' /> :
-                <Status style={{ width: '144px' }} status='isActive' />}
-              <Cell style={{ width: '144px' }}>{item.generated}</Cell>
-              <Cell style={{ width: '144px' }}>{item.changed}</Cell>
-              {item.blocked_changed === "unlock" ? <Button style={{ width: '174px' }} backgroundColor="#F7F7F7" >Разблокировать</Button> :
-                <Button style={{ width: '174px' }} backgroundColor="#F7F7F7"><img src={block} alt="" /> Заблокировать</Button>}
-            </Row>
-          )}
+          {
+            data && data.length > 0
+              ? data.map(({
+                id,
+                fullName,
+                age,
+                gender,
+                status,
+                createdAt,
+                updatedAt
+              }) => (
+                <Row key={id} className={id === selectPlayer ? 'selectPlayer' : 'playerField'} style={{ alignItems: 'center', height: '64px', paddingRight: '8px' }}
+                  onClick={() => { selectPlayer === id ? setSelectPlayer('') : setSelectPlayer(id) }}>
+                  <Cell style={{ width: "380px" }}>{fullName}</Cell>
+                  <Cell>{age}</Cell>
+                  {gender === "FEMALE" ? (
+                    <Cell style={{ width: '46px' }}>
+                      <img src={woman} alt="" />
+                    </Cell>
+                  ) : (
+                    <Cell style={{ width: '46px' }}>
+                      <img src={man} alt="" />
+                    </Cell>
+                  )}
+                  {status === "BLOCKED" ? (
+                    <Status style={{ width: '144px' }} status="isBlock" />
+                  ) : (
+                    <Status style={{ width: '144px' }} status="isActive" />
+                  )}
+                  <Cell style={{ width: '144px' }}>{formatToClientDate(createdAt)}</Cell>
+                  <Cell style={{ width: '144px' }}>{formatToClientDate(updatedAt)}</Cell>
+                  {status === "BLOCKED" ? (
+                    <Button
+                      onClick={() => handleClick(id)}
+                      style={{ width: '174px' }}
+                      backgroundColor="#F7F7F7"
+                    >
+                      Разблокировать
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={() => handleClick(id)}
+                      style={{ width: '174px' }}
+                      backgroundColor="#F7F7F7"
+                    >
+                      <img src={block} alt="" /> Заблокировать
+                    </Button>
+                  )}
+                </Row>
+              ))
+              : null
+          }
         </Container>
-      </Container>
+      </Container >
 
-      <ModalTemplate active={AddPlayerActive}>
-        <div style={{ display: 'flex', justifyContent: 'end' }} onClick={() => setAddPlayerActive(false)}><img src={close} alt="" /></div>
-        <AddPlayerModal />
-      </ModalTemplate>
+      <AddPlayerModal selectPlayer={selectPlayer} modalState={modalState} onPlayerAdded={handleAddPlayer} />
     </>
   )
 }
