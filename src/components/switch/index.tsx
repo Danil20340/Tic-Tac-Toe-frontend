0b290ddd4e158e0ba1899468e430cf3bc65d1efe
Даниил <144549184+Danil20340@users.noreply.github.com>
2024-12-10 03:00:41 +0500

    filter for active players

--- src/components/switch/index.tsx
+++ src/components/switch/index.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import './index.css'
 
-export const Switch = () => {
+type Props = {
+    isChecked: boolean,
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const Switch: React.FC<Props> = ({ isChecked, onChange }) => {
     return (
         <label className="switch">
-            <input type="checkbox" />
+            <input type="checkbox" checked={isChecked} onChange={onChange} />
             <span className="slider" />
         </label>
-    )
-}
+    );
+};
