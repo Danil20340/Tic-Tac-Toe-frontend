cd2825bc2f34a33fceeff14c4af1c2f22c34e90f
Даниил <144549184+Danil20340@users.noreply.github.com>
2024-12-10 03:02:30 +0500

    auto add current player

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,26 @@
-import React, { useEffect } from 'react'
-import { Container } from '../container'
+import React, { useEffect, useState } from 'react'
 import { NavBar } from '../nav-bar'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { logout, selectCurrent, selectIsAuthenticated } from '../../features/player/playerSlice'
+import { useLazyGetCurrentPlayerQuery } from '../../app/services/playerApi'
+import { useDispatch } from 'react-redux'
 
 export const Layout = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const navigate = useNavigate();
+  const current = useSelector(selectCurrent);
+  const [triggerGetCurrentPlayer] = useLazyGetCurrentPlayerQuery();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      dispatch(logout());
+      navigate("/auth");
+    } else if (!current) {
+      triggerGetCurrentPlayer();
+    }
+  }, [isAuthenticated, current, navigate, triggerGetCurrentPlayer]);
 
   return (
     <>
