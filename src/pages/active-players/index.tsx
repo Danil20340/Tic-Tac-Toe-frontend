51fbf65e87c445bf06cf86acc6338ddb04f8c783
Даниил <144549184+Danil20340@users.noreply.github.com>
2024-12-29 18:25:29 +0500

    retrieves data from the server

--- src/pages/active-players/index.tsx
+++ src/pages/active-players/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
 import { Container } from '../../components/container';
 import { Text } from '../../components/text';
@@ -8,26 +8,67 @@ import { Row } from '../../components/row';
 import { Cell } from '../../components/cell';
 import { Switch } from '../../components/switch';
 import { useGetAllPlayersQuery } from '../../app/services/playerApi';
-import { InviteGameModal } from '../../components/invite-game-modal';
-import { useModal } from '../../components/modal-context';
+import { useSelector } from 'react-redux';
+import { selectCurrent, selectSocket } from '../../features/player/playerSlice';
+import { useSocket } from '../../features/socket';
+import { useNotifications } from '../../components/notification-provider';
+import { AvailabilityStatus, Player } from '../../app/types';
+
+type PlayerData = {
+  id: string;
+  fullName: string;
+  availability: AvailabilityStatus;
+};
 export const ActivePlayers = () => {
+  const [players, setPlayers] = useState<PlayerData[]>([]);
   const [showAvailableOnly, setShowAvailableOnly] = useState(false); // Состояние для переключателя
-  const { data, refetch } = useGetAllPlayersQuery();
-  const { openModal } = useModal();
+  const { socket, isConnected } = useSocket();
+  // Получаем текущего игрока через useSelector
+  const currentPlayer = useSelector(selectCurrent);
+  const current = currentPlayer?.id;
+
   const handleSwitchChange = () => {
     setShowAvailableOnly(!showAvailableOnly);
-    refetch();
-
   };
-  const filteredData = data?.filter(player =>
-    !showAvailableOnly || player.availability === "AVAILABLE"
+
+  const handleInvite = (playerId: string) => {
+    if (socket && isConnected) {
+      console.log(`Приглашение отправлено игроку ${playerId}`);
+      socket.emit("invite", { fromPlayerId: current, toPlayerId: playerId });
+      console.log(`Игрок ${playerId} приглашен игроком ${current}`);
+    } else {
+      console.error("Сокет недоступен или не подключен");
+    }
+  };
+  useEffect(() => {
+    if (socket) {
+      socket.on("online-players", (playersData: [string, { fullName: string; availability: AvailabilityStatus; }][]) => {
+        console.log(playersData);
+
+        const formattedPlayers = playersData.map(([id, player]) => ({
+          id,
+          fullName: player.fullName, // Извлекаем строку fullName
+          availability: player.availability, // Извлекаем доступность
+        }));
+        setPlayers(formattedPlayers);
+      });
+      // Очистка обработчика при размонтировании компонента
+      return () => {
+        socket.off("online-players");
+      };
+    }
+  }, [socket]);
+
+  // Фильтрация данных
+  const filteredData = players.filter(
+    (player) => !showAvailableOnly || player.availability === AvailabilityStatus.AVAILABLE
   );
+  // console.log(players);
   return (
     <>
       <Container className="active-players">
         <Container style={{ justifyContent: 'space-between' }}>
           <Text style={{ fontWeight: 700, fontSize: "24px", lineHeight: '36px' }}>Активные игроки</Text>
-          <Button onClick={() => openModal('inviteModal')}>Открыть модалку</Button>
           <Container style={{ maxWidth: '200px', alignItems: 'center' }}>
             <Text style={{ marginRight: '8px' }}>Только свободные</Text>
             <Container style={{ maxWidth: '35px' }}>
@@ -42,22 +83,27 @@ export const ActivePlayers = () => {
                 ({
                   id,
                   fullName,
-                  availability,
-                  status
+                  availability
                 }) =>
-                  status === 'ACTIVE' &&
-                  <Row key={id} style={{ boxShadow: "none", padding: "12px 0px", gap: '16px' }}>
-                    <Cell style={{ width: "300px" }}>{fullName}</Cell>
-                    {availability === "AVAILABLE" ? <Status /> : <Status status='isPlay' />}
-                    {availability === "AVAILABLE" ? <Button >Позвать играть</Button> :
-                      <Button disabled={true} backgroundColor="#F7F7F7" >Позвать играть</Button>}
+                  <Row
+                    key={id}
+                    style={{ boxShadow: "none", padding: "12px 0px", gap: "16px" }}
+                  >
+                    <Cell style={{ width: "300px" }}>{fullName}</Cell>{availability === 'AVAILABLE' ? (<Status />
+                    ) : (
+                      <Status status="isPlay" />
+                    )}
+                    {availability === 'AVAILABLE' ? (<Button onClick={() => handleInvite(id)}>Позвать играть</Button>) :
+                      (
+                        <Button disabled={true} backgroundColor="#F7F7F7">Позвать играть</Button>
+                      )}
                   </Row>
               )
-              : null
+              : <p>Нет доступных игроков</p>
           }
+
         </Container>
       </Container>
-      <InviteGameModal />
     </>
   )
 }
--- src/pages/rating/index.tsx
+++ src/pages/rating/index.tsx
@@ -1,29 +1,12 @@
+import { useGetPlayerRatingsQuery } from '../../app/services/gameApi';
 import { Cell } from '../../components/cell';
 import { Container } from '../../components/container';
 import { Row } from '../../components/row';
 import { Text } from '../../components/text';
 import './index.css'
 
-type DataRow = {
-  [key: string]: string | number;
-}
 export const Rating = () => {
-  const data: DataRow[] = [
-    { FIO: 'Александров Игнат Анатолиевич', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Шевченко Рафаил Михайлович', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Мазайло Трофим Артёмович', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Логинов Остин Данилович', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Борисов Йошка Васильевич', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Соловьёв Ждан Михайлович', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Негода Михаил Эдуардович', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Гордеев Шамиль Леонидович', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Многогрешный Павел Виталиевич', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Александров Игнат Анатолиевич', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Волков Эрик Алексеевич', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Кузьмин Ростислав Васильевич', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Стрелков Филипп Борисович', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' },
-    { FIO: 'Галкин Феликс Платонович', games: "24534", wins: '9824', loss: '1222', percentage_of_wins: '87%' }
-  ];
+  const { data } = useGetPlayerRatingsQuery();
   return (
     <Container className="rating-players">
       <Text style={{ fontWeight: 700, fontSize: "24px", lineHeight: '36px' }}>Рейтинг игроков</Text>
@@ -33,17 +16,30 @@ export const Rating = () => {
           <Cell>Всего игр</Cell>
           <Cell>Победы</Cell>
           <Cell>Проигрыши</Cell>
+          <Cell>Ничьи</Cell>
           <Cell style={{ width: "138px" }}>Процент побед</Cell>
         </Row>
-        {data.map((item, index) => (
-          <Row key={index}>
-            <Cell style={{ width: "440px" }}>{item.FIO}</Cell>
-            <Cell>{item.games}</Cell>
-            <Cell style={{ color: "#69B849" }}>{item.wins}</Cell>
-            <Cell style={{ color: "#E93E3E" }}>{item.loss}</Cell>
-            <Cell style={{ width: "138px" }}>{item.percentage_of_wins}</Cell>
-          </Row>
-        ))}
+        {
+          data && data.length > 0
+            ? data.map(({
+              id,
+              playerName,
+              totalGames,
+              wins,
+              losses,
+              draw,
+              winPercentage
+            }) => (
+              <Row key={id}>
+                <Cell style={{ width: "440px" }}>{playerName}</Cell>
+                <Cell>{totalGames}</Cell>
+                <Cell style={{ color: "#69B849" }}>{wins}</Cell>
+                <Cell style={{ color: "#E93E3E" }}>{losses}</Cell>
+                <Cell style={{ color: "#87cae8" }}>{draw}</Cell>
+                <Cell style={{ width: "138px" }}>{winPercentage}</Cell>
+              </Row>
+            ))
+            : null}
       </Container>
     </Container>
   )
