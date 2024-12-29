37d2007fb3aee4de626ac5392758b802852b9195
Даниил <144549184+Danil20340@users.noreply.github.com>
2024-12-29 18:24:35 +0500

    socketIo

---
+++ src/features/socket.tsx
@@ -0,0 +1,59 @@
+import { io, Socket } from "socket.io-client";
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCurrent, selectIsAuthenticated } from "./player/playerSlice";
+import { getSocket } from "../utils/socketSingleton";
+
+const registeredPlayers: Set<string> = new Set(); // Глобальный набор
+
+export const useSocket = (): { socket: Socket | null; isConnected: boolean } => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const currentPlayer = useSelector(selectCurrent);
+  const socketRef = useRef<Socket | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    if (isAuthenticated && currentPlayer) {
+      if (!socketRef.current) {
+        socketRef.current = getSocket(); // WebSocket создается один раз
+      }
+
+      const socket = socketRef.current;
+
+      if (!socket.connected) {
+        socket.connect();
+      }
+
+      const handleConnect = () => {
+        setIsConnected(true);
+
+        if (!registeredPlayers.has(currentPlayer.id)) {
+          socket.emit("register", { playerId: currentPlayer.id, fullName: currentPlayer.fullName });
+          console.log("WebSocket подключен:", { playerId: currentPlayer.id });
+          registeredPlayers.add(currentPlayer.id); // Отмечаем как зарегистрированного
+        }
+      };
+
+      const handleDisconnect = () => {
+        setIsConnected(false);
+        console.log("WebSocket отключен");
+      };
+
+      socket.on("connect", handleConnect);
+      socket.on("disconnect", handleDisconnect);
+
+      socket.on("connect_error", (error) => {
+        console.error("Ошибка подключения:", error);
+      });
+
+      return () => {
+        socket.off("connect", handleConnect);
+        socket.off("disconnect", handleDisconnect);
+        socket.disconnect();
+        console.log("WebSocket отключен");
+      };
+    }
+  }, [isAuthenticated, currentPlayer]);
+
+  return { socket: socketRef.current, isConnected };
+};
