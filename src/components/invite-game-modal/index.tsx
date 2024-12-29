c733143f98d88fd54c0daec1d4a1b4a689e73184
Даниил <144549184+Danil20340@users.noreply.github.com>
2024-12-29 18:26:06 +0500

    invite modal

---
+++ src/components/invite-game-modal/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { Modal } from '../modal-template'
+import { Text } from '../text'
+import { Button } from '../button'
+import { Container } from '../container'
+import game from '../../assets/game.svg'
+import { useModal } from '../../components/modal-context';
+import { useLazyGetPlayerByIdQuery } from '../../app/services/playerApi'
+import { useSocket } from '../../features/socket'
+import { selectCurrent } from '../../features/player/playerSlice'
+import { useSelector } from 'react-redux'
+import { removeThirdWord } from '../../utils/remove-third-word'
+
+export const InviteGameModal = () => {
+    const { isModalOpen, closeModal, getModalData } = useModal();
+    const current = useSelector(selectCurrent);
+    const [fetchPlayer, { data: player }] = useLazyGetPlayerByIdQuery();
+    const { socket, isConnected } = useSocket();
+    const fromPlayerId = getModalData('inviteModal')?.fromPlayerId;
+
+    React.useEffect(() => {
+        if (isModalOpen('inviteModal') && fromPlayerId) {
+            fetchPlayer({ id: fromPlayerId });
+        }
+    }, [isModalOpen, fromPlayerId, fetchPlayer]);
+
+    const handleAccept = () => {
+        console.log('Приглашение принято от:', fromPlayerId);
+        closeModal('inviteModal');
+        console.log(player?.fullName);
+    };
+
+    const handleDecline = () => {
+        console.log('Приглашение отклонено от игрока:', current?.id, 'Отказ отправлен игроку:', player?.id);
+        closeModal('inviteModal');
+        if (socket && isConnected) {
+
+            socket.emit("decline", { fromPlayerId: current?.id, toPlayerId: player?.id });
+
+            return () => {
+                socket.off("decline");
+            };
+        }
+    };
+
+    return (
+        <Modal name='inviteModal'>
+            <Container>
+                <img style={{ width: '80%' }} src={game} alt="Game" />
+            </Container>
+            <Container style={{ flexDirection: 'column', gap: '20px' }}>
+                <Text style={{ fontWeight: 700, fontSize: '24px' }}>
+                    Игрок {removeThirdWord(player?.fullName)} приглашает вас в игру!
+                </Text>
+                <Container style={{ flexDirection: 'column', gap: '12px' }}>
+                    <Button onClick={handleAccept}>Принять</Button>
+                    <Button onClick={handleDecline} backgroundColor="#F7F7F7">
+                        Отклонить
+                    </Button>
+                </Container>
+            </Container>
+        </Modal>
+    );
+};
