479a81f1135642a82b4f710c3266deacc5b97887
Даниил <bronnikovds@gmail.com>
2025-02-17 22:42:08 +0500

    add ref proprerty

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -5,6 +5,7 @@ type Props = {
     children: React.ReactNode;
     style?: CSSProperties;
     className?: string;
+    ref?: React.Ref<HTMLDivElement>;
 }
 
 export const Container: React.FC<Props> = ({ children, style, className }) => {
